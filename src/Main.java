c8eb0757e1745202d9d01d033e3c06ff91dc9d92
Christopher Suhar <suharce@mail.uc.edu>
2024-09-23 21:15:59 -0400

    Lab_4_01 Completed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int itemPrice = 10;
+        double SalesTax = .05;
+
+        System.out.println("Your final price for this item is " + 10.5);
+    }
+}
